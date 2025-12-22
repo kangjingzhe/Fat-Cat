dd5c75e0e422965f137003c3790d09e7193b1467
answeryt <answeryt@qq.com>
2025-12-22 17:17:19 +0800

    Update __init__.py

--- MCP/__init__.py
+++ MCP/__init__.py
@@ -1,14 +1,10 @@
 # -*- coding: utf-8 -*-
 from ._mcp_function import MCPToolFunction
 from ._client_base import MCPClientBase
-from ._stateful_client_base import StatefulClientBase
 from ._http_stateless_client import HttpStatelessClient
-from ._stdio_stateful_client import StdIOStatefulClient
 
 __all__ = [
     'MCPToolFunction',
     'MCPClientBase', 
-    'StatefulClientBase',
     'HttpStatelessClient',
-    'StdIOStatefulClient'
 ]
